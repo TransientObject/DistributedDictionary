7c8e221cf0cdf9d8e921d55f0b88082535cdbe76
Priya PNS <mail2priyanarayanan@gmail.com>
2017-02-13 20:44:04 -0800

    First part of assignment 3

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -2,40 +2,72 @@ import java.io.*;
 import java.net.*;
 import java.lang.String;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class UDPServer
 {
-    public static void main(String args[]) throws Exception
-    {
+    public static void main(String args[]) throws Exception {
         DatagramSocket serverSocket = new DatagramSocket(9876);
         byte[] receiveData = new byte[1024];
-        byte[] sendMeaning = new byte[10000];
+        ExecutorService executor = Executors.newFixedThreadPool(5);
 
-        String response;
-        System.out.println("Server started. Waiting for input from client on port 9876\n");
-        while(true)
-        {
+
+        while(true){
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             String word = new String(receivePacket.getData(), 0, receivePacket.getLength());
-            Boolean isWord = false;
-            System.out.println("\n\nReceived input from client. Word is " + word);
 
             // ending condition
             if (word.equals("#")){
-            	System.out.println("Client is closing the session. Bye\n");
-                break;
+                System.out.println("Client is closing the session. Bye\n");
+                executor.shutdown();
+                while (!executor.isTerminated()) {}
+                System.out.println("All threads have finished executing. Server closing session");
+                serverSocket.close();
+                System.exit(0);
             }
+            Runnable getDataAndSend = new UDPServerThread(word, serverSocket, receivePacket);
+            executor.execute(getDataAndSend);
+        }
+    }
+}
+
+
+class UDPServerThread implements Runnable{
+    long threadID = -1;
+    String word;
+    DatagramPacket receivePacket;
+    DatagramSocket serverSocket;
+
+    UDPServerThread(String word, DatagramSocket serverSocket, DatagramPacket receivePacket){
+        this.word = word;
+        this.receivePacket = receivePacket;
+        this.serverSocket = serverSocket;
+    }
+
+    public void run(){
+        try{
+            this.threadID = Thread.currentThread().getId();
+            byte[] sendMeaning = new byte[10000];
+
+            String response;
+            System.out.println("ServerThread started. Waiting for input from client on port 9876\n");
+            System.out.println("Thread ID - " + this.threadID);
+
+            Boolean isWord = false;
+            System.out.println("\n\nReceived input from client. Word is " + word + " thread ID is " + this.threadID);
+
             InetAddress IPAddress = receivePacket.getAddress();
             int port = receivePacket.getPort();
-            
-            
+
+
             isWord = Dictionary.validateWord(word);
             if (isWord){
                 response = Dictionary.getMeaning(word);
                 if (response.isEmpty()){
-                	response = "Sorry, meaning was not found in the dictionary!";
-                	System.out.println(response + "\n");
+                    response = "Sorry, meaning was not found in the dictionary!";
+                    System.out.println(response + "\n");
                 }
             }
             else {
@@ -47,6 +79,8 @@ class UDPServer
             DatagramPacket sendPacket = new DatagramPacket(sendMeaning, sendMeaning.length, IPAddress, port);
             serverSocket.send(sendPacket);
         }
-        serverSocket.close();
+        catch(Exception e){
+            System.out.println("Exception caught : "+ e.getMessage());
+        }
     }
 }
